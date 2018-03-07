aa421e9ebe7d71b4ab79acc28b31d05ba565e619
Daniel Bradshaw <daniel.bradshaw@acuitybrands.com>
2018-03-07 07:32:17 -0700

    removes uneeded files

--- webpack.common.js
+++ webpack.common.js
@@ -26,27 +26,34 @@ module.exports = {
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
       // Loads URLs specified in CSS files
-      /*{
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['url-loader']
-      }*/
       {
         test:/\.(png|jpe?g|gif)$/,
-        exclude:/node_modules/
-        ,loader: 'url-loader?limit=1024&name=/assets/[name].[ext]'
+        exclude:/node_modules/,
+        loader: 'url-loader?limit=1024&name=/assets/[name].[ext]'
       }
     ]
   },
 
+  // Sometimes it's preferrable to keep 3rd party libraries external.  If this
+  // section is uncommented, you must place a script tag somewhere to import
+  // the library as the build process will skip bundling these external references
+  /*externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "mapbox-gl": "mapboxgl",
+    "chart.js": "Chart"
+  },*/
+
   plugins: [
+    // Inject the bundles into the distribution index.html
     new HtmlWebpackPlugin({
       inject: true,
       template: 'src/app.html',
       filename: 'index.html'
     }),
 
+    // Copy the data folder to build folder
     new CopyWebpackPlugin([
-      //{ from: 'assets', to: 'assets' },
       { from: 'data', to: 'data' }
     ])
   ]
